de40c63a9854c6474db55796a96a675445f2e8b4
Eshaanjeetcheema <s30261@caseygrammar.vic.edu.au>
2025-09-18 01:44:45 +0000

    Rename task 1 and fixed sample

--- Task3.py
+++ Task3.py
@@ -2,10 +2,11 @@
 # Assume that a dog year is 7 human years. 
 # Write a program that asks the user
 def main():
+
     #Write your code for input here
-    
-
-
+ ` dogYears = int(input("Dog years: "))
+humanYears = dogYears * 7
+print(f"Human years = {humanYears}")    
     # End of your code for input here
 
 if __name__ == '__main__':
